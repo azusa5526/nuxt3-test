01ea04f9366c9d5101c1ab21e6e4ba41049fc119
AndrewYao <andrewyao@anasystem.com.tw>
2023-06-20 13:32:22 +0800

    feat: 補上 main grow

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col h-screen">
     <AppHeader class="shrink-0 fixed" />
-    <main class="pt-[var(--app-header-height)]">
+    <main class="pt-[var(--app-header-height)] grow">
       <slot></slot>
     </main>
     <AppFooter class="shrink-0" />
