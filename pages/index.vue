c4981137931c16b8fa170d434052eb02e6dd2b29
AndrewYao <andrewyao@anasystem.com.tw>
2023-06-21 16:28:29 +0800

    feat: 主頁增加假圖片測試 header 透明效果

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,10 @@
 <template>
 	<div class="bg-yellow-200">
-		<h1>Hello index.vue</h1>
+		<img
+			src="https://dummyimage.com/1920x1280/e62260/fff"
+			class="max-h-[800px] min-h-[600px] w-full object-cover"
+			alt=""
+		/>
 		<NuxtLink to="/about" class="text-blue-500">About link</NuxtLink>
 		<div v-for="n in 10" :key="n" class="py-10">{{ n }}</div>
 		<SvgIcon use="account_circle" class="h-10 w-10 text-red-400"></SvgIcon>
