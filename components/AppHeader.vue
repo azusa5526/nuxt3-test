a1d9ec05a770659e544baca34a39a722b2784f02
AndrewYao <andrewyao@anasystem.com.tw>
2023-06-21 16:26:46 +0800

    feat: 加入 Header 置頂顏色動態更改

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,5 +1,8 @@
 <template>
-	<div class="flex h-[var(--app-header-height)] w-screen justify-between bg-green-400">
+	<div
+		class="flex h-[var(--app-header-height)] w-screen justify-between duration-300"
+		:class="isScrollPositionTop ? 'bg-transparent' : 'bg-white'"
+	>
 		<div class="flex">
 			<button @click.stop="isDrawerShow = !isDrawerShow" class="block bg-black/20 px-4 py-2 text-white lg:hidden">
 				Toggle
@@ -14,9 +17,25 @@
 <script lang="ts" setup>
 import { useAppStore } from '@/store/app';
 import { storeToRefs } from 'pinia';
+import { throttle } from 'lodash';
+import { useEventListener } from '@vueuse/core';
 
 const appStore = useAppStore();
 const { isDrawerShow } = storeToRefs(appStore);
+
+onMounted(() => {
+	useEventListener(document, 'scroll', testFunc);
+	isScrollPositionTop.value = document.body.getBoundingClientRect().y === 0;
+});
+
+const isScrollPositionTop = ref(true);
+const testFunc = throttle(() => {
+	document.body.getBoundingClientRect().y === 0
+		? (isScrollPositionTop.value = true)
+		: (isScrollPositionTop.value = false);
+
+	console.log(isScrollPositionTop.value);
+}, 200);
 </script>
 
 <style scoped></style>
