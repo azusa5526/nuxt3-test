421c9e6c403b683b8563ee9a72e93aa0eafc7905
AndrewYao <andrewyao@anasystem.com.tw>
2023-06-29 13:10:37 +0800

    feat: 增加 main mb

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
 	<div class="flex h-screen flex-col">
 		<AppSidebar class="z-50 block w-[--app-sidebar-width] bg-white lg:hidden" />
 		<AppHeader class="fixed z-40 shrink-0" />
-		<main class="grow">
+		<main class="mb-12 grow">
 			<slot></slot>
 		</main>
 		<AppFooter class="shrink-0" />
