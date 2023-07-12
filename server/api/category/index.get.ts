828643714023c4dcb1d1e50a27746dd817182025
AndrewYao <andrewyao@anasystem.com.tw>
2023-07-12 08:20:17 +0800

    feat: 刪除 category api 註解 code

--- server/api/category/index.get.ts
+++ server/api/category/index.get.ts
@@ -4,19 +4,6 @@ export default defineEventHandler(async () => {
 	try {
 		const categories = await Category.find();
 		return categories;
-		// return categories.map((category) => ({
-		// 	id: category._id,
-		// 	name: category.name,
-		// 	image_url: category.image_url,
-		// 	route: category.route,
-		// 	order: category.order,
-		// 	sub_categories: category.sub_categories.map((sub_categorie) => ({
-		// 		id: sub_categorie._id,
-		// 		name: sub_categorie.name,
-		// 		route: sub_categorie.route,
-		// 		order: sub_categorie.order,
-		// 	})),
-		// }));
 	} catch (error) {
 		console.error('get category err', error);
 	}
