4dddabb8ec29c7fd0d5be3fadf00416800e2243e
DESKTOP-FIJNG4G\Andrew <azusa5526@gmail.com>
2023-07-16 21:33:46 +0800

    chore: 刪除靜態 topics 資料

--- components/Topics.vue
+++ components/Topics.vue
@@ -38,51 +38,6 @@
 import { Topic } from '~/types';
 
 const topicsFetch = reactive(await useFetch<Topic[]>('/api/topics'));
-
-const topics = [
-	{
-		image_url: 'https://dummyimage.com/720X540/34eb83/fff',
-		category: 'CAMPAIGN',
-		title: '【オリジナルフォンタブ：プレゼント】',
-		contents:
-			'オンラインストア限定販売 ATH-SQ1TW STB の発売を記念して、Audio-Technicaオリジナルの『フォンタブ』ご用意しました。公式オンラインストアにて、ATH-SQ1TW STB(EGR／SBK／SWH)お買い上げのお客様にもれなくプレゼント。(数量限定)',
-	},
-	{
-		image_url: 'https://dummyimage.com/720X540/34eb83/fff',
-		category: 'CAMPAIGN',
-		title: '【オリジナルフォンタブ：プレゼント】',
-		contents:
-			'オンラインストア限定販売 ATH-SQ1TW STB の発売を記念して、Audio-Technicaオリジナルの『フォンタブ』ご用意しました。公式オンラインストアにて、ATH-SQ1TW STB(EGR／SBK／SWH)お買い上げのお客様にもれなくプレゼント。(数量限定)',
-	},
-	{
-		image_url: 'https://dummyimage.com/720X540/34eb83/fff',
-		category: 'CAMPAIGN',
-		title: '【オリジナルフォンタブ：プレゼント】',
-		contents:
-			'オンラインストア限定販売 ATH-SQ1TW STB の発売を記念して、Audio-Technicaオリジナルの『フォンタブ』ご用意しました。公式オンラインストアにて、ATH-SQ1TW STB(EGR／SBK／SWH)お買い上げのお客様にもれなくプレゼント。(数量限定)',
-	},
-	{
-		image_url: 'https://dummyimage.com/720X540/34eb83/fff',
-		category: 'CAMPAIGN',
-		title: '【オリジナルフォンタブ：プレゼント】',
-		contents:
-			'オンラインストア限定販売 ATH-SQ1TW STB の発売を記念して、Audio-Technicaオリジナルの『フォンタブ』ご用意しました。公式オンラインストアにて、ATH-SQ1TW STB(EGR／SBK／SWH)お買い上げのお客様にもれなくプレゼント。(数量限定)',
-	},
-	{
-		image_url: 'https://dummyimage.com/720X540/34eb83/fff',
-		category: 'CAMPAIGN',
-		title: '【オリジナルフォンタブ：プレゼント】',
-		contents:
-			'オンラインストア限定販売 ATH-SQ1TW STB の発売を記念して、Audio-Technicaオリジナルの『フォンタブ』ご用意しました。公式オンラインストアにて、ATH-SQ1TW STB(EGR／SBK／SWH)お買い上げのお客様にもれなくプレゼント。(数量限定)',
-	},
-	{
-		image_url: 'https://dummyimage.com/720X540/34eb83/fff',
-		category: 'CAMPAIGN',
-		title: '【オリジナルフォンタブ：プレゼント】',
-		contents:
-			'オンラインストア限定販売 ATH-SQ1TW STB の発売を記念して、Audio-Technicaオリジナルの『フォンタブ』ご用意しました。公式オンラインストアにて、ATH-SQ1TW STB(EGR／SBK／SWH)お買い上げのお客様にもれなくプレゼント。(数量限定)',
-	},
-];
 </script>
 
 <style>
