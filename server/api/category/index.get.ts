fc0f2513503958c96243f57f7ea1ddedbc8f67f1
DESKTOP-FIJNG4G\Andrew <azusa5526@gmail.com>
2023-07-17 20:03:06 +0800

    feat: 增加 category, sub_categories 排序

--- server/api/category/index.get.ts
+++ server/api/category/index.get.ts
@@ -2,7 +2,7 @@ import { Category } from '../../models/category';
 
 export default defineEventHandler(async () => {
 	try {
-		const categories = await Category.find();
+		const categories = await Category.find({}).sort({ order: 1, 'sub_categories.order': 1 });
 		return categories;
 	} catch (error) {
 		console.error('get category err', error);
