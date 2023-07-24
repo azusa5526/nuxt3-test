2ca03714472f0d7b037f336627af720b7eada51a
DESKTOP-FIJNG4G\Andrew <azusa5526@gmail.com>
2023-07-24 20:37:06 +0800

    feat: 修改 category slug 樣式

--- pages/category/[...slug].vue
+++ pages/category/[...slug].vue
@@ -1,14 +1,14 @@
 <template>
 	<div>
 		<div class="flex flex-col items-center">
-			<div class="mb-4 mt-2 text-sm">PRODUCTS</div>
-			<div class="mb-7 text-[3rem]">{{ categoryInfo?.category.name.toUpperCase() }}</div>
+			<div class="text-md mb-1 mt-3">PRODUCTS</div>
+			<div class="mb-7 text-[3.5rem]">{{ categoryInfo?.category.name.toUpperCase() }}</div>
 		</div>
 
-		<div class="flex flex-col bg-yellow-200">
+		<div class="flex flex-col border-b border-gray-400">
 			<span class="text-center">{{ categoryInfo?.category.name.toUpperCase() }}</span>
 
-			<div class="flex justify-between border-b border-gray-400">
+			<div class="mx-auto flex w-full max-w-[1600px] justify-between px-2">
 				<div class="flex">
 					<div class="mr-28">
 						<span class="mr-8">FILTERS</span>
@@ -25,31 +25,25 @@
 		</div>
 
 		<div class="mx-auto flex max-w-[1600px] pt-12">
-			<div class="w-[25vw] min-w-[210px] max-w-[300px] px-2">
+			<div class="hidden w-[25vw] min-w-[210px] max-w-[320px] px-2 md:block">
 				<span class="text-sm">{{ categoryInfo?.category.name.toUpperCase() }}</span>
 				<ul>
 					<li
 						v-for="subCategory in subCategoriesInfo"
 						:key="subCategory.id"
 						class="my-2.5 border border-gray-300 text-center text-sm"
-						:class="{ 'bg-gray-300 text-white': route.path === subCategory.route }"
+						:class="{ 'bg-[#BFBFBF] text-white': route.path === subCategory.route }"
 					>
-						<a :href="subCategory.route" class="block p-2">{{ subCategory.name }}</a>
+						<NuxtLink :to="subCategory.route" class="block p-2">{{ subCategory.name }}</NuxtLink>
 					</li>
 				</ul>
 			</div>
 
-			<div class="flex w-[75vw] max-w-[1032px] px-8">
-				<div class="grid grid-cols-3 place-content-start gap-0">
-					<div v-for="product in products" :key="product.id" class="testtest m-3 bg-red-200 p-20">
-						{{ product.name }}
-					</div>
-					<div v-for="product in products" :key="`${product.id}-2`" class="testtest m-3 bg-red-200 p-20">
-						{{ product.name }}
-					</div>
-					<div v-for="product in products" :key="`${product.id}-3`" class="testtest m-3 bg-red-200 p-20">
-						{{ product.name }}
-					</div>
+			<div class="flex w-full px-4 md:w-[75vw] md:px-8 lg:max-w-[1032px]">
+				<div class="grid grid-cols-2 place-content-start gap-0 md:grid-cols-3">
+					<ProductCard :product="product" v-for="product in products" :key="`${product.id}_1`"></ProductCard>
+					<ProductCard :product="product" v-for="product in products" :key="`${product.id}_2`"></ProductCard>
+					<ProductCard :product="product" v-for="product in products" :key="`${product.id}_3`"></ProductCard>
 				</div>
 			</div>
 		</div>
@@ -86,3 +80,15 @@ const subCategoriesInfo = computed(() => {
 	)?.sub_categories;
 });
 </script>
+
+<style scoped lang="scss">
+:deep(a > img) {
+	@media screen and (min-width: 1205px) {
+		padding: 0 2rem;
+	}
+}
+
+:deep(.product-card-name) {
+	margin-bottom: 2rem;
+}
+</style>
