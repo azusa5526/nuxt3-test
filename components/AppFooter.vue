e480acff66d1ab2c1da180afd42fd5c83e3e7501
DESKTOP-FIJNG4G\Andrew <azusa5526@gmail.com>
2023-07-24 20:36:10 +0800

    feat: 修改 footer layout

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,33 +1,36 @@
 <template>
 	<div class="w-100 flex flex-wrap bg-[#404040] px-16 pt-14 text-white">
-		<div class="mr-24 flex !h-10 items-center">
-			<PiniaIcon class="mx-0 h-10 w-10"></PiniaIcon>
-			<span class="text-lg">audio-Technica</span>
+		<div class="flex flex-wrap md:flex-nowrap">
+			<div class="mb:mb-0 mb-6 mr-24 flex !h-10 items-center">
+				<PiniaIcon class="mx-0 h-10 w-10"></PiniaIcon>
+				<span class="text-lg">audio-Technica</span>
+			</div>
+
+			<div class="mb-12 flex flex-wrap text-sm">
+				<div v-for="footerItem in footerItems" :key="footerItem.category" class="mb-12 mr-12 flex flex-col">
+					<div class="mb-6">{{ footerItem.category }}</div>
+					<ul>
+						<li v-for="menuItem in footerItem.menuItems" :key="menuItem.name">
+							<a href="#" class="leading-6 hover:opacity-70">{{ menuItem.name }}</a>
+						</li>
+					</ul>
+				</div>
+
+				<div class="mb-12 mr-12 flex flex-col">
+					<ul>
+						<li v-for="infoItem in infoItems" :key="infoItem.name">
+							<a href="#" class="leading-6 hover:opacity-70">{{ infoItem.name }}</a>
+						</li>
+					</ul>
+				</div>
+			</div>
 		</div>
 
-		<div class="mb-14 flex text-sm">
-			<div v-for="footerItem in footerItems" :key="footerItem.category" class="mr-14 flex flex-col">
-				<div class="mb-6">{{ footerItem.category }}</div>
-				<ul>
-					<li v-for="menuItem in footerItem.menuItems" :key="menuItem.name">
-						<a href="#" class="leading-6 hover:opacity-70">{{ menuItem.name }}</a>
-					</li>
-				</ul>
-			</div>
-			<div class="mr-10 flex flex-col">
-				<ul>
-					<li v-for="infoItem in infoItems" :key="infoItem.name">
-						<a href="#" class="leading-6 hover:opacity-70">{{ infoItem.name }}</a>
-					</li>
-				</ul>
-			</div>
-			<div class="flex">
-				<a href="#" v-for="n in 5" :key="n" class="mx-2">
-					<SvgIcon use="account_circle" class="h-9 w-9 hover:opacity-70" />
-				</a>
-			</div>
+		<div class="flex w-full justify-center">
+			<a href="#" v-for="n in 5" :key="n" class="mx-2">
+				<SvgIcon use="account_circle" class="h-9 w-9 hover:opacity-70" />
+			</a>
 		</div>
-
 		<div class="flex w-full justify-center p-5 text-sm">@ For practice use only.</div>
 	</div>
 </template>
@@ -85,8 +88,8 @@ const footerItems = [
 ];
 
 const infoItems = [
-	{ name: 'ソリューション・事例', query: '' },
-	{ name: '設備音響製品', query: '' },
-	{ name: '設備音響製品', query: '' },
+	{ name: '規約・規定', query: '' },
+	{ name: 'GDPRプライバシーポリシー', query: '' },
+	{ name: '特定商取引に関する法律に基づく表示', query: '' },
 ];
 </script>
