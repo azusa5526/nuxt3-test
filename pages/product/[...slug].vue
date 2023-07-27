377ad99665d4c42e70da27ff686c0c2921ecd5be
AndrewYao <andrewyao@anasystem.com.tw>
2023-07-27 16:29:20 +0800

    feat: 增加單一產品頁面 product/[...slug].vue 基礎頁

---
+++ pages/product/[...slug].vue
@@ -0,0 +1,16 @@
+<template>
+	<div>Product slug</div>
+</template>
+
+<script lang="ts" setup>
+import { useAppStore } from '~/store/app';
+
+definePageMeta({
+	name: 'product',
+});
+
+const appStore = useAppStore();
+const route = useRoute();
+
+console.log('route', route);
+</script>
