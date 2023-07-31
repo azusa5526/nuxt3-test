155a61ca46f106c9c112fff7d068e7ffb70873e0
DESKTOP-FIJNG4G\Andrew <azusa5526@gmail.com>
2023-08-01 01:20:11 +0800

    feat: 填充試驗用 md file

--- components/ProductFeature.vue
+++ components/ProductFeature.vue
@@ -1,11 +1,30 @@
 <template>
 	<div>
-		<span>ProductFeature.vue</span>
-		<ContentDoc path="/prod/ATH-M50x/feature" />
+		<ContentDoc class="feature-content" path="/prod/ATH-M50x/feature" />
 	</div>
 </template>
 
 <script lang="ts" setup>
-const contentQuery = await queryContent().find();
-console.log(contentQuery);
+// const contentQuery = await queryContent().find();
+// console.log(contentQuery);
 </script>
+
+<style lang="scss">
+.feature-content {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	h1 {
+		font-size: 36px;
+		font-weight: bold;
+	}
+
+	h4 {
+		font-weight: bold;
+	}
+
+	p {
+		max-width: 812px;
+	}
+}
+</style>
