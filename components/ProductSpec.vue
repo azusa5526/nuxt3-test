0abd511924e3930b316c704c912e0bbb05acd0b7
AndrewYao <andrewyao@anasystem.com.tw>
2023-08-07 10:14:42 +0800

    feat: 修改 ProductSpec 模板

--- components/ProductSpec.vue
+++ components/ProductSpec.vue
@@ -1,6 +1,6 @@
 <template>
-	<div>
-		<div v-if="product.technical_data?.spec.length" class="w-[900px] mx-auto mt-10 text-sm leading-6">
+	<div v-if="product.technical_data" class="w-[900px] mx-auto mt-10 text-sm leading-6">
+		<div v-if="product.technical_data?.spec.length">
 			<div v-for="spec in product.technical_data.spec" class="py-8 border-b border-[#BFBFBF] px-20">
 				<div v-for="item in spec.items">
 					<div v-if="spec.title" class="my-3 title-dot">{{ spec.title }}</div>
@@ -15,37 +15,37 @@
 					</div>
 				</div>
 			</div>
+		</div>
 
-			<div v-if="product.technical_data.info" class="py-8 border-b border-[#BFBFBF] px-20">
-				<div v-if="product.technical_data.info.accessories">
-					<div class="my-3 title-dot">付属品</div>
-					<div>{{ product.technical_data.info.accessories }}</div>
-				</div>
-				<div v-if="product.technical_data.info.sold_separately">
-					<div class="my-3 title-dot">別売</div>
-					<div>{{ product.technical_data.info.sold_separately }}</div>
-				</div>
+		<div v-if="product.technical_data.info" class="py-8 border-b border-[#BFBFBF] px-20">
+			<div v-if="product.technical_data.info.accessories">
+				<div class="my-3 title-dot">付属品</div>
+				<div>{{ product.technical_data.info.accessories }}</div>
 			</div>
-
-			<div class="py-10 border-b border-[#BFBFBF] px-20">
-				<div v-if="product.technical_data.notice">{{ product.technical_data.notice }}</div>
-				<div v-else>（ 改良などのため予告なく変更することがあります。）</div>
+			<div v-if="product.technical_data.info.sold_separately">
+				<div class="my-3 title-dot">別売</div>
+				<div>{{ product.technical_data.info.sold_separately }}</div>
 			</div>
+		</div>
 
-			<div class="grid grid-cols-1  md:grid-cols-3 [&>div]:p-3 text-center border-b border-[#BFBFBF] mb-[276px]">
-				<div class="border-r border-[#BFBFBF]">
-					<div class="mb-5">型番</div>
-					<div v-for="branch in product.branches" :key="branch.id">{{ branch.model }}</div>
-				</div>
-				<div class="border-r border-[#BFBFBF]">
-					<div class="mb-5">JANコード</div>
-					<div v-for="branch in product.branches" :key="branch.id">{{ branch.jan_code }}</div>
-				</div>
-				<div>
-					<div class="mb-5">発売日</div>
-					<div v-for="branch in product.branches" :key="branch.id"> {{ branch.release_at ?
-						getFormattedDate(branch.release_at) : '-' }}</div>
-				</div>
+		<div class="py-10 border-b border-[#BFBFBF] px-20">
+			<div v-if="product.technical_data.notice">{{ product.technical_data.notice }}</div>
+			<div v-else>（ 改良などのため予告なく変更することがあります。）</div>
+		</div>
+
+		<div class="grid grid-cols-1  md:grid-cols-3 [&>div]:p-3 text-center border-b border-[#BFBFBF] mb-[276px]">
+			<div class="border-r border-[#BFBFBF]">
+				<div class="mb-5">型番</div>
+				<div v-for="branch in product.branches" :key="branch.id">{{ branch.model }}</div>
+			</div>
+			<div class="border-r border-[#BFBFBF]">
+				<div class="mb-5">JANコード</div>
+				<div v-for="branch in product.branches" :key="branch.id">{{ branch.jan_code }}</div>
+			</div>
+			<div>
+				<div class="mb-5">発売日</div>
+				<div v-for="branch in product.branches" :key="branch.id"> {{ branch.release_at ?
+					getFormattedDate(branch.release_at) : '-' }}</div>
 			</div>
 		</div>
 	</div>
