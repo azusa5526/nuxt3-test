0ccadf8feb8568d674c8e45b6279ca4a163194e3
AndrewYao <andrewyao@anasystem.com.tw>
2023-08-22 15:59:01 +0800

    feat: 修改 DetailContent 樣式

--- components/content/DetailContent.vue
+++ components/content/DetailContent.vue
@@ -1,20 +1,23 @@
 <template>
 	<div class="w-full">
-		<div @click="toggle" class="mx-auto max-w-[1090px] cursor-pointer select-none pb-6 pt-10 text-center font-bold">
-			<div class="relative" :class="$attrs['sub-title'] ? 'mb-7' : 'mb-5'">
-				<div class="grow text-4xl">{{ $attrs.title }}</div>
+		<div
+			@click="toggle"
+			class="mx-auto max-w-[1090px] cursor-pointer select-none px-5 pb-6 pt-6 text-center font-bold ct_md:pt-[44px]"
+		>
+			<div class="relative" :class="$attrs['sub-title'] ? 'mb-4 ct_md:mb-7' : 'ct_md:mb-5'">
+				<div class="grow text-xl ct_md:text-4xl">{{ $attrs.title }}</div>
 				<SvgIcon
-					class="absolute -top-1 right-6 h-12 w-12 justify-self-end transition-all duration-300"
+					class="absolute -top-1 right-0 h-10 w-10 justify-self-end transition-all duration-300 ct_md:right-6 ct_md:h-12 ct_md:w-12"
 					:class="{ '-rotate-180': isExpend }"
 					use="expand_more"
 				></SvgIcon>
 			</div>
-			<div>{{ $attrs['sub-title'] }}</div>
+			<div class="text-xs ct_md:text-base">{{ $attrs['sub-title'] }}</div>
 		</div>
 		<div
 			ref="contentRef"
 			class="overflow-hidden transition-[max-height] duration-300"
-			:class="{ 'mb-8': isExpend }"
+			:class="{ 'ct_md:mb-8': isExpend }"
 			:style="{ maxHeight: `${maxHeight}` }"
 		>
 			<ContentSlot :use="$slots.default" />
